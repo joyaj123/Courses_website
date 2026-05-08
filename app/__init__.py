54ee80a9419663508b610f4ff464e08f3af0f1ab
diamariasfeir <diamariasfeir@gmail.com>
2026-05-08 14:25:09 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,48 @@
 from flask import Flask
 
+# import routes
+from app.routes.login import login
+from app.routes.signup import signup
+from app.routes.user_routes import get_me
+from app.routes.courses_routes import get_my_courses
+from app.routes.course_details import course_details
+
+from app.routes.admin_course_routes import (
+    add_course,
+    edit_course,
+    remove_course,
+    get_course,
+    get_course_edit
+)
+
 def create_app():
     app = Flask(__name__)
 
+    # HOME
     @app.route("/")
     def home():
         return "Hello World!"
+
+    # AUTH
+    app.add_url_rule("/login", view_func=login, methods=["POST"])
+    app.add_url_rule("/signup", view_func=signup, methods=["POST"])
+
+    # USER
+    app.add_url_rule("/me", view_func=get_me, methods=["GET"])
+
+    # COURSES
+    app.add_url_rule("/my-courses", view_func=get_my_courses, methods=["GET"])
+    app.add_url_rule("/courses/<int:course_id>", view_func=course_details, methods=["GET"])
+
+    # ADMIN
+    app.add_url_rule('/admin/course', view_func=add_course, methods=['POST'])
+
+    app.add_url_rule('/admin/course/<int:course_id>', view_func=edit_course, methods=['PUT'])
+
+    app.add_url_rule('/admin/course/<int:course_id>', view_func=remove_course, methods=['DELETE'])
+
+    app.add_url_rule('/admin/course/<int:course_id>', view_func=get_course, methods=['GET'])
+
+    app.add_url_rule('/admin/course/edit/<int:course_id>', view_func=get_course_edit, methods=['GET'])
 
     return app
